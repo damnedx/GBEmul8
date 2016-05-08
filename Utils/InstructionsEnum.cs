159c50b375806ef85f86cb05fac0b2ac34cf5ff8
damnedx <daskow67@gmail.com>
2016-05-08 21:32:07 +0200

    Ajout d'enumeration

--- Utils/InstructionsEnum.cs
+++ Utils/InstructionsEnum.cs
@@ -8,21 +8,28 @@ namespace GBEmul8.Utils
 {
     class InstructionsEnum
     {
-        public enum OPCodeEnum
+        public enum OPCodeHeaderEnum
         {
+            // OP header
             SYS = 0x0,
-            CLS = 0x00E0,
-            RET = 0x00EE,
             JUMP = 0x1,
             CALL = 0x2,
-            SE_KK = 0x3,
-            SNE = 0x4,
+            SE_CONST = 0x3,
+            SNE_CONST = 0x4,
             SE_XY = 0x5,
-            LD_KK = 0x6,
-            ADD_KK = 0x7,
-            LD_XY = 0x8
+            LD_CONST = 0x6,
+            ADD_CONST = 0x7,
+            LD_XY = 0x8,
+            SNE_XY = 0x9
 
 
+
+        }
+
+        public enum OPCodeInstructionsEnum
+        {
+            CLS = 0x00E0,
+            RET = 0x00EE,
         }
     }
 }
