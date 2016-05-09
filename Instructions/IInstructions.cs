864ac7460178aacc6a12e2fbd60dc641ee700a91
damnedx <daskow67@gmail.com>
2016-05-09 14:57:32 +0200

    Ajout de nouvelles instructions CHIP8

--- Instructions/IInstructions.cs
+++ Instructions/IInstructions.cs
@@ -12,10 +12,13 @@ namespace GBEmul8.Instructions
         void ret(ref ushort _PC, ref Stack<ushort> SP);
 
         // Saute a l'addresse N
-        void Jump_Address(ushort N, ref ushort PC);
+        void JUMPTO(ushort N, ref ushort PC);
         
         // appel l'instruction a l'addresse N
-        void Call(ushort N, ref ushort PC, ref Stack<ushort> SP);
+        void CALL(ushort N, ref ushort PC, ref Stack<ushort> SP);
+
+
+        // ARITHMETIQUE
 
         // Test si V == Const
         void SE(byte V, byte Const, ref ushort PC);
@@ -28,6 +31,37 @@ namespace GBEmul8.Instructions
 
         // Affecte KK à V
         void LD_CONST(ref byte V, byte Const);
+
+        // Affecte V = V + KK
+        void ADD_CONST(ref byte V, byte Const);
+
+        // V = Y
+        void LD_XY(ref byte V, ref byte Y);
+
+        // V = V OR Y
+        void OR(ref byte V, ref byte Y);
+
+        // V = V & Y
+        void AND(ref byte V, ref byte Y);
+
+        // V = V ^ Y
+        void XOR(ref byte V, ref byte Y);
+
+        void ADD_XY(ref byte V, ref byte Y, ref byte F);
+
+        void SUB_XY(ref byte V, ref byte Y, ref byte F);
+
+        void SHR(ref byte V, ref byte F);
+
+        void SUB_N(ref byte V, ref byte Y, ref byte F);
+
+        void LD_ADDR(ref ushort I, ref ushort N);
+
+        void JUMP_ADDR(ref byte V, ref ushort N, ref ushort PC);
+
+        void RND(ref byte V, ref byte Const);
+
+
 
     }
 }
--- Instructions/SystInstructions.cs
+++ Instructions/SystInstructions.cs
@@ -13,6 +13,7 @@ namespace GBEmul8.Chip8
     class SystInstructions : IInstructions
     {
         ushort _opcode;
+        private Random _rnd = new Random();
         public SystInstructions(ushort opcode)
         {
             this._opcode = opcode;
@@ -35,12 +36,12 @@ namespace GBEmul8.Chip8
         {
             _PC = SP.Pop();
         }
-        public void Jump_Address(ushort N, ref ushort PC)
+        public void JUMPTO(ushort N, ref ushort PC)
         {
             PC = N;
         }
 
-        public void Call(ushort N, ref ushort PC, ref Stack<ushort> SP)
+        public void CALL(ushort N, ref ushort PC, ref Stack<ushort> SP)
         {
             // On enregistre l'instruction actuelle dans la pile
             SP.Push(PC);
@@ -69,6 +70,88 @@ namespace GBEmul8.Chip8
         public void LD_CONST(ref byte V, byte Const)
         {
             V = Const;
+        }
+
+        public void ADD_CONST(ref byte V, byte Const)
+        {
+            V += Const;
+        }
+
+        public void LD_XY(ref byte V, ref byte Y)
+        {
+            V = Y;
+        }
+
+        public void OR(ref byte V, ref byte Y)
+        {
+            V |= Y;
+        }
+        public void AND(ref byte V, ref byte Y)
+        {
+            V &= Y;
+        }
+        public void XOR(ref byte V, ref byte Y)
+        {
+            V ^= Y;
+        }
+
+        public void ADD_XY(ref byte V, ref byte Y, ref byte F)
+        {
+            int tmp = V + Y;
+            if (tmp > 0xFF)
+                F = 0x1;
+            else
+                F = 0x0;
+            V = (byte)(tmp % 0xFF);
+        }
+
+        public void SUB_XY(ref byte V, ref byte Y, ref byte F)
+        {
+            if (V > Y)
+                F = 0x1;
+            else
+                F = 0x0;
+            V -= Y;
+        }
+        public void SHR(ref byte V, ref byte F)
+        {
+            F = (byte)(V & 0x1);
+            V >>= 1;
+        }
+
+        public void SUB_N(ref byte V, ref byte Y, ref byte F)
+        {
+            if (Y > V)
+                F = 0x1;
+            else
+                F = 0x0;
+            V = (byte)(Y - V);
+        }
+
+        public void SHL(ref byte V, ref byte F)
+        {
+            F = (byte)(V >> 7);
+            V <<= 1;
+          
+        }
+
+        public void SNE_XY(byte V, byte Y, ref ushort PC)
+        {
+            if (V != Y)
+                PC += 2; // on passe a l'instruction suivante
+        }
+        public void LD_ADDR(ref ushort I, ref ushort N)
+        {
+            I = N;
+        }
+
+        public void JUMP_ADDR(ref byte V, ref ushort N, ref ushort PC)
+        {
+            PC = (ushort)(V + N);
+        }
+        public void RND(ref byte V, ref byte Const)
+        {
+            V = (byte)(_rnd.Next(0, 256) & Const);
         }
     }
 }
